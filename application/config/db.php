74e31657a6b40c5d9e3777c413b4a4c0b27cac4a
Khoa Phung <khoab1812275@student.ctu.edu.vn>
2021-06-08 02:01:00 +0700

    Update db.php

--- application/config/db.php
+++ application/config/db.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-	'host' => '103.124.94.177',
-	'name' => 'zhs_khoathpdf1w',
-	'user' => 'khoathpdf1w_zhs',
-	'password' => 'cx2bzczveUrC9cK',
+	'host' => '127.0.0.1',
+	'name' => 'CT428',
+	'user' => 'root',
+	'password' => '',
 ];
